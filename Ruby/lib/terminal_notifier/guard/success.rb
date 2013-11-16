48b6d4bc39d0fed7746bb93a4e270eb0dc19ecbb
Jamil Bou Kheir <jamilboukheir@me.com>
2013-11-16 12:04:43 -0800

    follow symlinks

--- Ruby/lib/terminal_notifier/guard/success.rb
+++ Ruby/lib/terminal_notifier/guard/success.rb
@@ -1,7 +1,19 @@
 module TerminalNotifier
   module Guard
     module Success
+<<<<<<< Updated upstream
       BIN_PATH = File.expand_path('../../../../vendor/terminal-notifier-failed.app/Contents/MacOS/terminal-notifier', __FILE__)
+=======
+      terminal_notifier_path = `which terminal-notifier`
+      if File.symlink? terminal_notifier_path
+        terminal_notifier_path = File.expand_path(File.readlink(terminal_notifier_path), __FILE__)
+      end
+
+      BIN_PATH = terminal_notifier_path.empty? ?
+        File.expand_path('../../../../vendor/terminal-notifier-failed.app/Contents/MacOS/terminal-notifier', __FILE__)
+        :
+        terminal_notifier_path
+>>>>>>> Stashed changes
     end
   end
 end
