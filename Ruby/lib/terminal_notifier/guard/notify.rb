0da066ccda93606079a66de6bf154079e8d1883b
Jamil Bou Kheir <jamilboukheir@me.com>
2013-11-16 11:23:40 -0800

    use terminal-notifier binary if detected via which

--- Ruby/lib/terminal_notifier/guard/notify.rb
+++ Ruby/lib/terminal_notifier/guard/notify.rb
@@ -1,7 +1,11 @@
 module TerminalNotifier
   module Guard
     module Notify
-      BIN_PATH = File.expand_path('../../../../vendor/terminal-notifier-notify.app/Contents/MacOS/terminal-notifier', __FILE__)
+      terminal_notifier_path = `which terminal-notifier`
+      BIN_PATH = terminal_notifier_path.empty? ?
+        File.expand_path('../../../../vendor/terminal-notifier-failed.app/Contents/MacOS/terminal-notifier', __FILE__)
+        :
+        terminal_notifier_path
     end
   end
 end
