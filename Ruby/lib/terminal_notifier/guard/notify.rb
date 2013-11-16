1c8d67599224a4b6b847c022e93fffd615ef5bfe
Jamil Bou Kheir <jamilboukheir@me.com>
2013-11-16 12:06:06 -0800

    remove forced upstream debris

--- Ruby/lib/terminal_notifier/guard/notify.rb
+++ Ruby/lib/terminal_notifier/guard/notify.rb
@@ -1,9 +1,6 @@
 module TerminalNotifier
   module Guard
     module Notify
-<<<<<<< Updated upstream
-      BIN_PATH = File.expand_path('../../../../vendor/terminal-notifier-failed.app/Contents/MacOS/terminal-notifier', __FILE__)
-=======
       terminal_notifier_path = `which terminal-notifier`
       if File.symlink? terminal_notifier_path
         terminal_notifier_path = File.expand_path(File.readlink(terminal_notifier_path), __FILE__)
@@ -13,7 +10,6 @@ module TerminalNotifier
         File.expand_path('../../../../vendor/terminal-notifier-failed.app/Contents/MacOS/terminal-notifier', __FILE__)
         :
         terminal_notifier_path
->>>>>>> Stashed changes
     end
   end
 end
