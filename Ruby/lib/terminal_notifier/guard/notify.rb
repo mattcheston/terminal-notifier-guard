44e802d93745f9bad7b8e2a19ac056e6b8fed327
Jamil Bou Kheir <jamilboukheir@me.com>
2013-11-16 12:21:31 -0800

    strip newline, no need to follow symlink

--- Ruby/lib/terminal_notifier/guard/notify.rb
+++ Ruby/lib/terminal_notifier/guard/notify.rb
@@ -1,10 +1,7 @@
 module TerminalNotifier
   module Guard
     module Notify
-      terminal_notifier_path = `which terminal-notifier`
-      if File.symlink? terminal_notifier_path
-        terminal_notifier_path = File.expand_path(File.readlink(terminal_notifier_path), __FILE__)
-      end
+      terminal_notifier_path = `which terminal-notifier`.strip
 
       BIN_PATH = terminal_notifier_path.empty? ?
         File.expand_path('../../../../vendor/terminal-notifier-failed.app/Contents/MacOS/terminal-notifier', __FILE__)
